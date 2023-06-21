2997e1f9421cb50990359ac0fa94cb213b9ce919
Lars S <58134413+Deaf-Wolf@users.noreply.github.com>
2023-06-21 20:48:47 +0200

    Bot Base

---
+++ DeafBot/bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+TOKEN = os.getenv('TOKEN')
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
