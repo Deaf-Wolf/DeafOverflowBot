b2112bc33ab95d1dade26f29f81b3ac51a56c86e
Lars S <58134413+Deaf-Wolf@users.noreply.github.com>
2023-10-12 21:08:26 +0200

    Added !Help command

--- DeafBot/main.py
+++ DeafBot/main.py
@@ -20,7 +20,13 @@ class MyClient(discord.Client):
             command = message.content[1:]  # Entfernt das '!'
             if command == 'hallo':
                 await message.channel.send('Hallo!')
-
+            elif command == 'help':
+                help_message = """
+                **Liste aller Befehle:**
+                `!hallo` - Sagt Hallo!
+                `!help` - Zeigt diese Hilfe an.
+                """
+                await message.channel.send(help_message)
         
         # prints every messages in console 
         else:
